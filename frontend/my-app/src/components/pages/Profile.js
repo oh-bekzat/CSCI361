c4f5b583b79331fedab05e9b4506f1503bc83080
yerzhanyerbatyr <yerzhan.english@gmail.com>
2023-11-20 03:09:13 +0600

    Profile fixed

--- frontend/my-app/src/components/pages/Profile.js
+++ frontend/my-app/src/components/pages/Profile.js
@@ -21,16 +21,17 @@ const Profile = () => {
     <div className="profile-container">
       <div className="left-section">
         {/* Profile Photo */}
-        <div className="profile-photo-container">
-          <img
-            src={userData?.profilePhotoUrl || 'https://placekitten.com/200/200'}
-            alt="Profile"
-            className="profile-photo"
-          />
-          <div className="user-name-container">
+        <div className="profile-info-container">
+          <div className="profile-photo-container">
+            <img
+              src={userData?.profilePhotoUrl || 'https://placekitten.com/200/200'}
+              alt="Profile"
+              className="profile-photo"
+            />
+            <div className="user-name-container">
             <h2>{userData?.name || 'User Name'}</h2>
-            <img src={require('../assets/star.png')} style={{ width: '20pt' }}  />
-            {/* <div className='body-20'><img src={require('../assets/star.png')} /></div> */}
+            <img src={require('../assets/star.png')} style={{ width: '20pt' }} alt="Star" />
+          </div>
           </div>
         </div>
 
