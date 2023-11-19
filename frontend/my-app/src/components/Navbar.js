ee1ca3bb2e8987ef7aa076b4cdedc367d9d58308
yerzhanyerbatyr <yerzhan.english@gmail.com>
2023-11-19 22:40:06 +0600

    Navbar update

--- frontend/my-app/src/components/Navbar.js
+++ frontend/my-app/src/components/Navbar.js
@@ -1,23 +1,21 @@
-import React, { useState, useEffect } from 'react';
-//import { Button } from './Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link, Route } from "react-router-dom";
 import './Navbar.css';
 
+const Navbar = () => {
+  return (
+    <header>
+        <a href="/">
+            <div className="header-32-bold">VMS Project</div>
+        </a>
+        <div className="navbar">
+            <a href="./">Home</a>
+            <a href="./Services">Services</a>
+            <a href="./vehicle-info">Vehicle Info</a>
+            <a href="./history">History</a>
+        </div>
+    </header>
+  );
+};
 
-function Navbar(){
-    return (
-        <>
-            <nav className='navbar'>
-            <div className='navbar-container'>
-                <Link to='/' > 
-                    Home
-            
-                 </Link>
-            </div>
-            </nav>
-            <div>fkkv</div>
-        </>
-    )
-}
-
-export default Navbar 
+export default Navbar;
