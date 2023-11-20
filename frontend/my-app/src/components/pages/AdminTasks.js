57dce9b323c7fb401da4a38f57ff1cc84ab5cdee
sdilnaz <dilnaz.sembekova@gmail.com>
2023-11-20 22:58:14 +0600

    admin task update

--- frontend/my-app/src/components/pages/AdminTasks.js
+++ frontend/my-app/src/components/pages/AdminTasks.js
@@ -3,14 +3,19 @@ import React, { useState, useEffect } from 'react';
 import './AdminTasks.css';
 import axios from 'axios';
 
-const AdminTasks = ({driverId}) => {
+import { Link } from 'react-router-dom';
+
+const AdminTasks = ({UserId}) => {
   // Sample data for assigned tasks
 //   const [assignedTasks, setAssignedTasks] = useState([]);
 //   const [selectedTask, setSelectedTask] = useState(null);
 
+
+
+// user_id, maintenance_description, maintenance_date, maintenance_cost, images
   const [assignedTasks, setAssignedTasks] = useState([
-    { id: 1, title: 'Route 1', details: 'Details for Task 1', date: '20.11.2023', time: '10:00', departure: 'Point A', arrival: 'Point B', status: 'Active', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-    { id: 2, title: 'Route 2', details: 'Details for Task 2', date: '21.11.2023', time: '13:00', departure: 'Point A', arrival: 'Point B', status: 'Active', description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+    { id: 1, title: 'Task 1', details: 'Details for Task 1', date: '13.11.2023', time: '10:00',  description: 'Fuel this car until next week', type : 'Fueling',assignedPerson:'John Doe', vehicleId:'P198JJN', status:'active'},
+    { id: 2, title: 'Task 2', details: 'Details for Task 2', date: '24.11.2023', time: '13:00', description: 'Requires ordering new objects' ,type : 'Maintence',assignedPerson:'Emily Thompson',  vehicleId:'T129NNN', status:'active'},
   ]);
   // State to track the selected task
   const [selectedTask, setSelectedTask] = useState(null);
@@ -41,7 +46,12 @@ const AdminTasks = ({driverId}) => {
 
 
   return (
-    <div className="driver-home-page">
+    
+    <div className="driver-home-page-button">
+      <Link to="/">
+        <button className="button-124">Add Task</button>
+      </Link>
+      <div className="driver-home-page">
       <div className="task-list">
         <ul>
           {assignedTasks.map((task) => (
@@ -50,10 +60,10 @@ const AdminTasks = ({driverId}) => {
                 <div className='body-20-bold'>{task.title}</div>
               </div>
               <div>
-                <span className='label-11-bold' style={{ marginLeft: '20px' }}>Point of departure: </span> <span className='label-11'>{task.departure}</span>
+                <span className='label-11-bold' style={{ marginLeft: '20px' }}>Type </span> <span className='label-11'>{task.type}</span>
               </div>
               <div>
-                <span className='label-11-bold' style={{ marginLeft: '20px' }}>Point of arrival: </span> <span className='label-11'>{task.departure}</span>
+                <span className='label-11-bold' style={{ marginLeft: '20px' }}>Date: </span> <span className='label-11'>{task.date}</span>
               </div>
             </li>
           ))}
@@ -74,25 +84,25 @@ const AdminTasks = ({driverId}) => {
                 <span className='body-14-bold'>Time: </span> <span className='body-14'>{selectedTask.time}</span>
             </div>
             <div>
-                <span className='body-14-bold'>Point of departure: </span> <span className='body-14'>{selectedTask.departure}</span>
+                <span className='body-14-bold'>Vehicle: </span> <span className='body-14'>{selectedTask.vehicleId}</span>
             </div>
-            <div>
-                <span className='body-14-bold'>Point of arrival: </span> <span className='body-14'>{selectedTask.arrival}</span>
-            </div>
+            
             <div>
                 <span className='body-14-bold'>Status: </span> <span className='body-14'>{selectedTask.status}</span>
             </div>
             <div>
+                <span className='body-14-bold'>Assigned Person: </span> <span className='body-14'>{selectedTask.assignedPerson}</span>
+            </div>
+            <div>
                 <span className='body-14-bold'>Description: </span> <span className='body-14'>{selectedTask.description}</span>
             </div>
-            <div className="button-container">
-              <button onClick={handleAssignTask}>Assign Task</button>
-            </div>
+            
           </>
         ) : (
           <div className='body-24'>Select a route to view details.</div>
         )}
       </div>
+    </div>
     </div>
   );
 };
