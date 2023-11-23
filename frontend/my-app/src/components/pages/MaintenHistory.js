cf0a51fdfec0d9d6486b268b580cbe20ee95aa41
sdilnaz <dilnaz.sembekova@gmail.com>
2023-11-24 04:09:41 +0600

    fueling tasks and maintenance history update

--- frontend/my-app/src/components/pages/MaintenHistory.js
+++ frontend/my-app/src/components/pages/MaintenHistory.js
@@ -5,27 +5,24 @@ import axios from 'axios';
 const MaintenHistory = ({maintenance_id}) => {
   // const [assignedTasks, setAssignedTasks] = useState([]);
   const [selectedTask, setSelectedTask] = useState(null);
-  const maintenanceId = localStorage.getItem("maintenance_id");
+  const maintenId = localStorage.getItem("maintenId");
 
 
-  const [assignedTasks, setAssignedTasks] = useState([
-    { id: 1, title: 'Maintenance Task 1', details: 'Details for Task 1', vehicleId: "A125FH", maintenance_date : '11.02.2023', maintenance_cost:"12000 kzt", maintenance_point:'СТО PRO' },
-    { id: 2, title: 'Maintenance Task 2', details: 'Details for Task 2', vehicleId: "S776JKN", maintenance_date : '21.11.2023', maintenance_cost:"7500 kzt", maintenance_point:'CTO 777'}
-  ]);
+  const [assignedTasks, setAssignedTasks] = useState([]);
 
-  // useEffect(() => {
-  //   // Fetch data from the localhost:3001/get-routes endpoint
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get(`http://localhost:3001/fuelling/${fuelingId}`);
-  //       console.log(response.data);
-  //       setAssignedTasks(response.data.routes);
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    // Fetch data from the localhost:3001/get-routes endpoint
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/maintenance/${maintenId}`);
+        console.log(response.data);
+        setAssignedTasks(response.data.routes);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   // Function to handle task selection
   const handleTaskSelection = (task) => {
@@ -39,7 +36,7 @@ const MaintenHistory = ({maintenance_id}) => {
           {Array.isArray(assignedTasks) && assignedTasks.map((task) => (
             <li key={task.id} onClick={() => handleTaskSelection(task)}>
               <div>
-                <div className='body-20-bold'>Maintenance Task {task.fuelingId}</div>
+                <div className='body-20-bold'>Maintenance Task {task.maintenId}</div>
               </div>
               <div>
                 <span className='label-11-bold' style={{ marginLeft: '20px' }}>Licence Plate: </span> <span className='label-11'>{task.vehicleId}</span>
@@ -63,7 +60,7 @@ const MaintenHistory = ({maintenance_id}) => {
         {selectedTask ? (
             <>
           <div>
-            <div className='body-24-bold'>Maintenance Task {selectedTask.fuelingId}</div>
+            <div className='body-24-bold'>Maintenance Task {selectedTask.maintenId}</div>
           </div>
             <div>
                 <span className='body-14-bold'>Licence Plate: </span> <span className='body-14'>{selectedTask.vehicleId}</span>
